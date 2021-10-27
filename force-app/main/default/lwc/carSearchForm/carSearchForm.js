f8ca43b07c3f259215611151a1ec587b40ed71f4
kratika2709 <chouhan.kratika2709@gmail.com>
2021-10-27 17:45:30 +0530

    Edited Code

--- force-app/main/default/lwc/carSearchForm/carSearchForm.js
+++ force-app/main/default/lwc/carSearchForm/carSearchForm.js
@@ -1,25 +1,49 @@
 import { LightningElement,track ,wire} from 'lwc';
 import getCarTypes from '@salesforce/apex/CarSearchFormController.getCarTypes';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+import {NavigationMixin} from 'lightning/navigation';
 
-export default class CarSearchForm extends LightningElement {
-    @track carTypes;
+export default class CarSearchForm extends NavigationMixin(LightningElement) {
+@track carTypes;
 
-    @wire(getCarTypes)
-    wiredCarType({data,error}){
-        if(data){
-            this.carTypes=[{value:'',label:'All Types'}];
-            data.forEach(element => {
-                const carType={};
-                carType.label = element.name;
-                carType.value = element.id;
-                this.carTypes.push(carType);
-            });
-        }
+@wire(getCarTypes)
+wiredCarType({data,error}){
+    if(data){
+        this.carTypes=[{value:'',label:'All Types'}];
+        data.forEach(element => {
+            const carType={};
+            carType.label = element.name;
+            carType.value = element.id;
+            this.carTypes.push(carType);
+        });
+    } else if (error){
+         this.ShowToast('ERROR',error.body.message,'error');
     }
-    handleCarTypeChange(event){
+}
 
-    }
-    createNewCarType(){
+handleCarTypeChange(event){
+    const carTypeId = event.detail.value;
+    const CarTypeSelectionChangeEvent = new CustomEvent('cartyoeselect',{
+        detail:carTypeId});
+        this.dispatchEvent(CarTypeSelectionChangeEvent);
+}
 
-    }
+createNewCarType(){
+      this[NavigationMixin.Navigate]({
+          type:'Standard_ObjectPage',
+          attributes:{
+              objectApiName :'Car_Type__c',
+              actionName: 'New'
+          }
+      })
+}
+
+ShowToast(title,massage,variant){
+    const evt= new ShowToastEvent({
+        title:title,
+        message:message,
+        variant:variant,
+    });
+    this.dispatchEvent(evt);
+}
 }
